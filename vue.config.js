a051c475a68f79b8735a0a6a588a086905b12408
Tessho Nakamura <tesstesstex@gmail.com>
2021-02-01 08:08:24 +0900

    Updated webpack configs

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,22 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
+
 module.exports = {
   configureWebpack: {
-    plugins: []
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+        inlineSource: '.(vue|js|css)',
+      }),
+      new HtmlWebpackInlineSourcePlugin(),
+      new CopyWebpackPlugin([
+        {
+          from: path.resolve(__dirname, '../gas'),
+          ignore: ['.*']
+        }
+      ])
+    ]
   }
 };
