547f3375989ad00c1e7f8986ee2e620f309b5ecc
Tessho Nakamura <tesstesstex@gmail.com>
2021-02-01 19:49:43 +0900

    Added url-loader for compiling imgs

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,34 @@
-const path = require('path')
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
 
 module.exports = {
   publicPath: '/public',
   chainWebpack: config => {
-    config
-      .plugin('html')
-      .tap(args => {
-        args[0].template = './public/index.html'
-        args[0].inlineSource = '.(vue|js|css)'
-        return args
-      })
+    config.plugin('html').tap(args => {
+      args[0].template = './public/index.html';
+      args[0].inlineSource = '.(vue|js|css)';
+      return args;
+    });
+    config.module
+      .rule('images')
+      .use('url-loader')
+      .options({});
+    config.module
+      .rule('media')
+      .use('url-loader')
+      .options({});
+    config.module
+      .rule('fonts')
+      .use('url-loader')
+      .options({});
+    config.module
+      .rule('svg')
+      .uses.delete('file-loader')
+      .end()
+      .use('url-loader')
+      .loader('url-loader')
+      .options({});
   },
   configureWebpack: {
     plugins: [
